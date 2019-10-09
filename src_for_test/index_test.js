da8b921d374b6424d1e5821f4f0e3a8b5c4261cd
emadurandal <emadurandal@gmail.com>
2019-10-09 15:06:09 +0900

    chore: Rename shader files

--- src_for_test/index_test.js
+++ src_for_test/index_test.js
@@ -1,5 +1,5 @@
-import simpleFragmentShader from '../assets/simple.frag';
-import simpleVertexShader from '../assets/simple.vert';
+import simpleFragmentShader from '../assets/simple_es3.frag';
+import simpleVertexShader from '../assets/simple_es3.vert';
 
 exports.simpleFragment = simpleFragmentShader;
 exports.simpleVertex = simpleVertexShader;
