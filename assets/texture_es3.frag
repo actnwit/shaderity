4e2b101353f794829831bd490b70b00ee7c654c3
KaYaF <d.guitar.c@gmail.com>
2021-08-16 18:46:03 +0900

    test: add test of texture3D and texture3DProj transform from GLSL ES3 to GLSL ES1

--- assets/texture_es3.frag
+++ assets/texture_es3.frag
@@ -1,10 +1,13 @@
 in vec2 v_texcoord;
 in vec3 v_texcoord3;
 uniform sampler2D u_texture;
+uniform sampler3D u_texture_3D;
 uniform samplerCube u_textureCube;
 
 void main (void) {
   gl_FragColor = texture(u_texture, v_texcoord);
   gl_FragColor = texture(u_textureCube, v_texcoord3);
   gl_FragColor = textureProj(u_texture, v_texcoord);
+  gl_FragColor = texture(u_texture_3D, v_texcoord);
+  gl_FragColor = textureProj(u_texture_3D, v_texcoord);
 }
--- test/basic.test.js
+++ test/basic.test.js
@@ -67,12 +67,15 @@ test('convert to ES1 correctly (texture)', async() => {
   expect(shaderity.transformTo('WebGL1', textureFragmentES3).code).toBe(`varying vec2 v_texcoord;
 varying vec3 v_texcoord3;
 uniform sampler2D u_texture;
+uniform sampler3D u_texture_3D;
 uniform samplerCube u_textureCube;
 
 void main (void) {
   gl_FragColor = texture2D(u_texture, v_texcoord);
   gl_FragColor = textureCube(u_textureCube, v_texcoord3);
   gl_FragColor = texture2DProj(u_texture, v_texcoord);
+  gl_FragColor = texture3D(u_texture_3D, v_texcoord);
+  gl_FragColor = texture3DProj(u_texture_3D, v_texcoord);
 }
 `);
 });
