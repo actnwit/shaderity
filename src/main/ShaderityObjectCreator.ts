a27ba6ec47490117146a0f26b57efe23c8830cea
KaYaF <d.guitar.c@gmail.com>
2021-08-23 17:49:06 +0900

    feat: add addExtension method

--- src/main/ShaderityObjectCreator.ts
+++ src/main/ShaderityObjectCreator.ts
@@ -1,4 +1,5 @@
 import {
+	ShaderExtensionBehavior,
 	ShaderExtensionObject,
 	ShaderityObject,
 	ShaderStageStr
@@ -23,6 +24,20 @@ export default class ShaderityObjectCreator {
 
 	constructor(shaderStage: ShaderStageStr) {
 		this.__shaderStage = shaderStage;
+	}
+
+	public addExtension(extensionName: string, behavior: ShaderExtensionBehavior = 'require') {
+		const isDuplicate =
+			this.__extensions.some(extension => extension.extensionName === extensionName);
+		if (isDuplicate) {
+			console.warn('addExtension: this extension is already set');
+			return;
+		}
+
+		this.__extensions.push({
+			extensionName,
+			behavior
+		});
 	}
 
 	public createShaderityObject(): ShaderityObject {
