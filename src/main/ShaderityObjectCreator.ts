405559f82ab4a1cad38edbde48b146dca4233e2e
KaYaF <d.guitar.c@gmail.com>
2021-08-23 14:14:41 +0900

    feat: add shaderity object creation method

--- src/main/ShaderityObjectCreator.ts
+++ src/main/ShaderityObjectCreator.ts
@@ -1,8 +1,24 @@
-import {ShaderStageStr} from '../types/type';
+import {ShaderityObject, ShaderStageStr} from '../types/type';
 
 export default class ShaderityObjectCreator {
 	private __shaderStage: ShaderStageStr;
+
 	constructor(shaderStage: ShaderStageStr) {
 		this.__shaderStage = shaderStage;
+	}
+
+	public createShaderityObject(): ShaderityObject {
+		const shaderityObj = {
+			code: this.__createShaderCode(),
+			shaderStage: this.__shaderStage,
+			isFragmentShader: this.__shaderStage === 'fragment',
+		};
+
+		return shaderityObj;
+	}
+
+	private __createShaderCode(): string {
+		// TODO: implementation
+		return '';
 	}
 }
