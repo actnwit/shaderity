71083eb124bb91ef527ce68cfc779efc24552ecb
KaYaF <d.guitar.c@gmail.com>
2021-08-24 14:55:37 +0900

    test: add test for creating shader code for global precisions part

--- test/basic.test.js
+++ test/basic.test.js
@@ -345,3 +345,32 @@ precision highp sampler2DArrayShadow;
 
 `);
 });
+
+test('test updateGlobalPrecision method in ShaderityObjectCreator', async() => {
+  const shaderityObjectCreator = Shaderity.createShaderityObjectCreator('vertex');
+  shaderityObjectCreator.updateGlobalPrecision({
+    int: 'mediump',
+    float: 'lowp',
+  });
+
+  const resultShaderityObj = shaderityObjectCreator.createShaderityObject();
+  expect(resultShaderityObj.code).toStrictEqual(`precision mediump int;
+precision lowp float;
+precision highp sampler2D;
+precision highp samplerCube;
+precision highp sampler3D;
+precision highp sampler2DArray;
+precision highp isampler2D;
+precision highp isamplerCube;
+precision highp isampler3D;
+precision highp isampler2DArray;
+precision highp usampler2D;
+precision highp usamplerCube;
+precision highp usampler3D;
+precision highp usampler2DArray;
+precision highp sampler2DShadow;
+precision highp samplerCubeShadow;
+precision highp sampler2DArrayShadow;
+
+`);
+});
