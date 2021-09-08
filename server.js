8294eb083cccd9845cc103c4404dfd7c42f36e1c
KaYaF <d.guitar.c@gmail.com>
2021-09-08 10:52:21 +0900

    build: add packages to create samples on a browser

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+express.static.mime.define({ 'application/javascript': ['mjs'] });
+const app = express();
+app.use(express.static('./'));
+
+const port = 8084;
+app.listen(port, () => {
+  console.log('Express server is running at http://localhost:' + port);
+});
