4e63313d7d77f5ec5ad337f01eef3c12a8d70058
KaYaF <d.guitar.c@gmail.com>
2021-09-14 11:11:24 +0900

    chore: export types of shaderity object creator

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,65 @@
 import Shaderity from './main/Shaderity';
+
 import {
-  AttributeSemantics,
-  ReflectionAttribute,
-  ReflectionUniform,
-  ReflectionVarying,
-  ShaderityObject,
-  ShaderExtensionBehavior,
-  ShaderConstantValueVarTypeES3,
-  ShaderPrecisionObject,
-  ShaderStageStr,
-  ShaderPrecisionType,
-  ShaderAttributeVarType,
-  ShaderVaryingInterpolationType,
-  ShaderVaryingVarType,
-  ShaderUniformVarTypeES3,
-  ShaderStructMemberObject,
-  ShaderUBOVariableObject,
-  ShaderVersion,
-  TemplateObject,
-  UniformSemantics,
-  VarType,
+  AttributeSemantics as _AttributeSemantics,
+  ReflectionAttribute as _ReflectionAttribute,
+  ReflectionUniform as _ReflectionUniform,
+  ReflectionVarying as _ReflectionVarying,
+  ShaderityObject as _ShaderityObject,
+  ShaderExtensionBehavior as _ShaderExtensionBehavior,
+  ShaderConstantValueVarTypeES3 as _ShaderConstantValueVarTypeES3,
+  ShaderPrecisionObject as _ShaderPrecisionObject,
+  ShaderStageStr as _ShaderStageStr,
+  ShaderPrecisionType as _ShaderPrecisionType,
+  ShaderAttributeVarType as _ShaderAttributeVarType,
+  ShaderVaryingInterpolationType as _ShaderVaryingInterpolationType,
+  ShaderVaryingVarType as _ShaderVaryingVarType,
+  ShaderUniformVarTypeES3 as _ShaderUniformVarTypeES3,
+  ShaderStructMemberObject as _ShaderStructMemberObject,
+  ShaderUBOVariableObject as _ShaderUBOVariableObject,
+  ShaderAttributeObject as _ShaderAttributeObject,
+  ShaderConstantStructValueObject as _ShaderConstantStructValueObject,
+  ShaderConstantValueObject as _ShaderConstantValueObject,
+  ShaderExtensionObject as _ShaderExtensionObject,
+  ShaderStructDefinitionObject as _ShaderStructDefinitionObject,
+  ShaderUniformBufferObject as _ShaderUniformBufferObject,
+  ShaderUniformObject as _ShaderUniformObject,
+  ShaderUniformStructObject as _ShaderUniformStructObject,
+  ShaderVaryingObject as _ShaderVaryingObject,
+  ShaderVersion as _ShaderVersion,
+  TemplateObject as _TemplateObject,
+  UniformSemantics as _UniformSemantics,
+  VarType as _VarType,
 } from './types/type';
 
-export type {
-  AttributeSemantics,
-  ReflectionAttribute,
-  ReflectionUniform,
-  ReflectionVarying,
-  ShaderityObject,
-  ShaderExtensionBehavior,
-  ShaderConstantValueVarTypeES3,
-  ShaderPrecisionObject,
-  ShaderStageStr,
-  ShaderPrecisionType,
-  ShaderAttributeVarType,
-  ShaderVaryingInterpolationType,
-  ShaderVaryingVarType,
-  ShaderUniformVarTypeES3,
-  ShaderStructMemberObject,
-  ShaderUBOVariableObject,
-  ShaderVersion,
-  TemplateObject,
-  UniformSemantics,
-  VarType,
-}
+export type AttributeSemantics = _AttributeSemantics;
+export type ReflectionAttribute = _ReflectionAttribute;
+export type ReflectionUniform = _ReflectionUniform;
+export type ReflectionVarying = _ReflectionVarying;
+export type ShaderityObject = _ShaderityObject;
+export type ShaderExtensionBehavior = _ShaderExtensionBehavior;
+export type ShaderConstantValueVarTypeES3 = _ShaderConstantValueVarTypeES3;
+export type ShaderPrecisionObject = _ShaderPrecisionObject;
+export type ShaderStageStr = _ShaderStageStr;
+export type ShaderPrecisionType = _ShaderPrecisionType;
+export type ShaderAttributeVarType = _ShaderAttributeVarType;
+export type ShaderVaryingInterpolationType = _ShaderVaryingInterpolationType;
+export type ShaderVaryingVarType = _ShaderVaryingVarType;
+export type ShaderUniformVarTypeES3 = _ShaderUniformVarTypeES3;
+export type ShaderStructMemberObject = _ShaderStructMemberObject;
+export type ShaderUBOVariableObject = _ShaderUBOVariableObject;
+export type ShaderAttributeObject = _ShaderAttributeObject;
+export type ShaderConstantStructValueObject = _ShaderConstantStructValueObject;
+export type ShaderConstantValueObject = _ShaderConstantValueObject;
+export type ShaderExtensionObject = _ShaderExtensionObject;
+export type ShaderStructDefinitionObject = _ShaderStructDefinitionObject;
+export type ShaderUniformBufferObject = _ShaderUniformBufferObject;
+export type ShaderUniformObject = _ShaderUniformObject;
+export type ShaderUniformStructObject = _ShaderUniformStructObject;
+export type ShaderVaryingObject = _ShaderVaryingObject;
+export type ShaderVersion = _ShaderVersion;
+export type TemplateObject = _TemplateObject;
+export type UniformSemantics = _UniformSemantics;
+export type VarType = _VarType;
 
 export default Shaderity
